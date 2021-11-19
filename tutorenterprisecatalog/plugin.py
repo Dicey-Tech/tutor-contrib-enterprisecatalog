6a303f0a3ca6e950052e9701ad93189ee29fdc2c
Sofiane Bebert <s.bebert@diceytech.co.uk>
2021-11-19 16:47:38 +0000

    feat: :sparkles: add container image

--- tutorenterprisecatalog/plugin.py
+++ tutorenterprisecatalog/plugin.py
@@ -4,20 +4,23 @@ import pkg_resources
 
 from .__about__ import __version__
 
-templates = pkg_resources.resource_filename(
-    "tutorenterprisecatalog", "templates"
-)
+templates = pkg_resources.resource_filename("tutorenterprisecatalog", "templates")
 
-config = {}
+config = {
+    "defaults": {
+        "VERSION": __version__,
+        "DOCKER_IMAGE": "{{ DOCKER_REGISTRY }}diceytech/enterprise-catalog:{{ ENTERPRISECATALOG_VERSION }}",
+    }
+}
 
-hooks = {}
+hooks = {
+    "build-image": {"enterprisecatalog": "{{ ENTERPRISECATALOG_DOCKER_IMAGE }}"},
+}
 
 
 def patches():
     all_patches = {}
-    patches_dir = pkg_resources.resource_filename(
-        "tutorenterprisecatalog", "patches"
-    )
+    patches_dir = pkg_resources.resource_filename("tutorenterprisecatalog", "patches")
     for path in glob(os.path.join(patches_dir, "*")):
         with open(path) as patch_file:
             name = os.path.basename(path)
